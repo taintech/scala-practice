a275e7508abd12e36f696637d6ad5a9e5076229f
Rinat <rinatsdu@gmail.com>
2011-08-01 00:06:00 +0600

    draft of third problem

--- src/euler/project/problem_3/LargestPrimeFactor.scala
+++ src/euler/project/problem_3/LargestPrimeFactor.scala
@@ -1,5 +1,8 @@
 package euler.project.problem_3
 
+import scala.math._
+import java.math.BigInteger
+
 /**
  * Created by IntelliJ IDEA.
  * User: tain
@@ -9,18 +12,45 @@ package euler.project.problem_3
  */
 
 object LargestPrimeFactor extends App{
-  def primity(n: Long): Boolean = primity_tr(n,n/2)
-  def primity_tr(n: Long, m: Long): Boolean = m match {
+  def primitivePrime(n: Int) = primitivePrime_tr(n,n/2)
+  def primitivePrime_tr(n: Int, m: Int): Boolean = m match {
     case 1 => true
     case _ if(n%m==0) => false
-    case _ => primity_tr(n,m-1)
+    case _ => primitivePrime_tr(n,m-1)
   }
-  def large(n: Long): Long = large_tr(n,n/2)
-  def large_tr(n: Long, m: Long): Long = m match {
-    case 1 => 1
-    case _ if(n%m==0&&primity(m)) => m
-    case _ => large_tr(n,m-1)
+
+  val listPrimitivePrime = for (i <- 2 to 1000 if primitivePrime(i)) yield i
+  println(listPrimitivePrime)
+  println(listPrimitivePrime.size)
+  //6*n+1 or 6*n-1
+  //increment with 2 and 4
+  def smallestFactor(n: Long) = smallestFactor_tr(n,2)
+  def smallestFactor_tr(n: Long, m: Long): Long = m match {
+    case _ if n%m==0 => m
+    case _ => smallestFactor_tr(n,m+1)
   }
-  println(primity(600851475143L))
-//  println(large(600851475143L))
+//  println(smallestFactor(600851475143L))
+  val inc = Pair(2,4)
+  val div = 600851475143L/2
+  val div1 = 13195/2
+  def gpfPrimitive(n: Long) = gpfPrimitive_tr(n,3,1,0)
+  def gpfPrimitive_tr(n: Long, m: Long, id: Long, ans: Long): Long = id match {
+    case _ if id>100 => ans
+    case _ if n%m==0&&id%2==0 => gpfPrimitive_tr(n,m+inc._1,id+1,m)
+    case _ if n%m==0&&id%2==1 => gpfPrimitive_tr(n,m+inc._2,id+1,m)
+    case _ if n%m==1&&id%2==0 => gpfPrimitive_tr(n,m+inc._1,id+1,ans)
+    case _ => gpfPrimitive_tr(n,m+inc._2,id+1,ans)
+  }
+  println(gpfPrimitive(13195))
+//  val a1: BigInt = 203
+//  val a1: BigInt = 207
+  val a1: BigInt = 199
+//  val a2: BigInt = 201
+//  val a2: BigInt = 209
+  val a2: BigInt = 215
+
+  println(a1.isProbablePrime(100))
+  println(a2.isProbablePrime(100))
+  println()
+//  println(gpfPrimitive(13195))
 }
