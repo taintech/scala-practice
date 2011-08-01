0e203fbc2a478b89249bdb2f869d3d092c5c14a4
Rinat Tainov <rinattainov@gmail.com>
2011-08-01 10:00:22 +0600

    solved problem_4 started 5

---
+++ src/euler/project/problem_5/SumDiff.scala
@@ -0,0 +1,14 @@
+package euler.project.problem_5
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Tain
+ * Date: 01.08.11
+ * Time: 9:58
+ * To change this template use File | Settings | File Templates.
+ */
+
+object SumDiff extends App{
+  val squareSum = pow((1+10)*5,2)
+  println(squareSum)
+}
