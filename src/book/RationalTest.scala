7c1ce80e6d51474ac3b40207baa28f2f8dac6a92
Rinat Tainov <rinattainov@gmail.com>
2011-09-13 11:19:59 +0600

    changed rational

--- src/book/RationalTest.scala
+++ src/book/RationalTest.scala
@@ -9,36 +9,25 @@ package book
  */
 
 object RationalTest extends App{
-//  val a = new Rational(2,3)
-//  val b = new Rational(3,4)
-//  println(a)
-//  println(b)
-//  val c = a + b
-//  val d = a - b
-////  val e = a / e
-//  println(c)
-
-
-
-//  val a = new Rational(1,2)
-//  val b = new Rational(5,2)
-//  val c = new Rational(2,4)
-//  val d = new Rational(7)
-//  println(a + " + " + b + " = " + (a + b))
-//  println(a + " + " + 2 + " = " + (a + 2))
-//  println(a + " - " + b + " = " + (a - b))
-//  println(a + " - " + 2 + " = " + (a - 2))
-//  println(a + " * " + c + " = " + (a * c))
-//  println(a + " * " + 2 + " = " + (a * 2))
-//  println(a + " / " + c + " = " + (a / c))
-//  println(a + " / " + 2 + " = " + (a / 2))
-//  println(a + " + " + b + " * " + c + " = " + (a + b * c))
-//  println(b + " max " + d + " = " + (b max d))
-//  println(b + " < " + d + " = " + (b < d))
-//  println(b + " > " + d + " = " + (b > d))
-//  println(b + " >= " + d + " = " + (b >= d))
-//  println(b + " <= " + d + " = " + (b <= d))
-//  println(b + " == " + d + " = " + (b == d))
-//  implicit def intToRational(x: Int) = new Rational(x)
-//  println(2 + " * " + a + " = " + (2 * a))
+  val a = new Rational(1,2)
+  val b = new Rational(5,2)
+  val c = new Rational(2,4)
+  val d = new Rational(7)
+  println(a + " + " + b + " = " + (a + b))
+  println(a + " + " + 2 + " = " + (a + 2))
+  println(a + " - " + b + " = " + (a - b))
+  println(a + " - " + 2 + " = " + (a - 2))
+  println(a + " * " + c + " = " + (a * c))
+  println(a + " * " + 2 + " = " + (a * 2))
+  println(a + " / " + c + " = " + (a / c))
+  println(a + " / " + 2 + " = " + (a / 2))
+  println(a + " + " + b + " * " + c + " = " + (a + b * c))
+  println(b + " max " + d + " = " + (b max d))
+  println(b + " < " + d + " = " + (b < d))
+  println(b + " > " + d + " = " + (b > d))
+  println(b + " >= " + d + " = " + (b >= d))
+  println(b + " <= " + d + " = " + (b <= d))
+  println(b + " == " + d + " = " + (b == d))
+  implicit def intToRational(x: Int) = new Rational(x)
+  println(2 + " * " + a + " = " + (2 * a))
 }
